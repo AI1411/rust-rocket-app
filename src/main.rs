e9ec554ad8d90c616aad508418fe4fe903b81d1c
AI1411 <goethe0719@gmail.com>
2021-03-09 22:16:49 +0900

    仮のCRUD機能実装

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,51 @@
-#[macro_use] extern crate rocket;
-#[macro_use] extern crate rocket_contrib;
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
 
 use rocket_contrib::json::JsonValue;
+use rocket::response::status;
 
 #[get("/")]
-fn hello() ->  JsonValue {
+fn hello() -> JsonValue {
     json!("hello world")
+}
+
+#[get("/rustaceans")]
+fn get_rustaceans() -> JsonValue {
+    json!([{"id": 1, "name": "Akira Ishii", "email": "akira@gmail.com"}, {"id": 2, "name": "sakura tomita", "email": "tomita@gmail.com"}])
+}
+
+#[get("/rustaceans/<id>")]
+fn view_rustaceans(id: i32) -> JsonValue {
+    json!({"id": id, "name": "Akira Ishii", "email": "akira@gmail.com"})
+}
+
+#[post("/rustaceans", format = "json")]
+fn create_rustaceans() -> JsonValue {
+    json!({"id": 3, "name": "sakura", "email": "test@gmail.com"})
+}
+
+#[put("/rustaceans/<id>", format = "json")]
+fn update_rustaceans(id: i32) -> JsonValue {
+    json!({"id": id, "name": "akira ishii", "email": "ishii@gmail.com"})
+}
+
+#[delete("/rustaceans/<id>")]
+fn delete_rustacean(id: i32) -> status::NoContent {
+    status::NoContent
 }
 
 #[rocket::main]
 async fn main() {
     let _ = rocket::ignite()
-        .mount("/", routes![hello])
+        .mount("/", routes![
+            get_rustaceans,
+            view_rustaceans,
+            create_rustaceans,
+            update_rustaceans,
+            delete_rustacean,
+        ])
         .launch()
         .await;
 }
